f351d60d44e9db26e2951502727e846f42697247
JeffBordenave <gbordenave@artfx.fr>
2023-10-03 18:18:04 +0200

    Initial commit

---
+++ Source.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+
+std::vector<int> SelectionSort(std::vector<int> v);
+void DisplayVector(std::vector<int> v);
+
+int main()
+{
+	std::cout << "Hello world!" << std::endl;
+
+	std::vector<int> a{ 7,5,4,9,2,12,2 };
+
+	DisplayVector(a);
+	a = SelectionSort(a);
+	DisplayVector(a);
+
+	return 0;
+}
+
+std::vector<int> SelectionSort(std::vector<int> v)
+{
+	bool isListSorted = false;
+	int currentIndexCheck = 0;
+	int min = 0;
+	int temp = 0;
+
+	while (!isListSorted)
+	{
+		min = currentIndexCheck;
+
+		for (int i = currentIndexCheck; i < v.size(); i++)
+		{
+			if (v[i] < v[min]) 
+				min = i;
+		}
+
+		temp = v[min];
+		v[min] = v[currentIndexCheck];
+		v[currentIndexCheck] = temp;
+
+		if (currentIndexCheck++ >= v.size()-1) return v;
+	}
+}
+
+void DisplayVector(std::vector<int> v)
+{
+	int size = v.size() - 1;
+
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << v[i] << ", ";
+	}
+
+	std::cout << v[size] << std::endl;
+}
